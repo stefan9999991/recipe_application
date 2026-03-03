eea386c93ef7c6e4fe5a056cf1792d6ce3011b7b
s3002985 <s.l.morrien@student.utwente.nl>
2026-03-03 12:04:27 +0100

    Creation of project

---
+++ src/test/java/com/example/recipe/recipe_application/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe.recipe_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
